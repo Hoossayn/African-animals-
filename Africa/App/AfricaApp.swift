e95559c9dda7f8e42809beec05a6483d4b4b8f2b
hoossayn <abdulhakeemhusseni@gmail.com>
2022-12-20 00:24:16 +0100

    initial commit: setting up assets

---
+++ Africa/App/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by hoossayn on 19/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
